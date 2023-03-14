f16579c2a9fbcba77a845de001d6212b96214229
aashutoshnishad147@gmail.com <ashutoshnishad2003@gmail.com>
2023-03-14 23:43:54 +0530

    Changing the web 2

--- Routes/Curd.js
+++ Routes/Curd.js
@@ -6,11 +6,17 @@ const router = express.Router();
 
 router.post("/add" ,async (req,res)=>{
     
-
+ try {
+    if(req.body.Name == undefined){
+            return res.send("Add Data please");
+    }
     var st1 = new Student(req.body)
     var rsp = await st1.save();
-
     res.send(rsp);
+ } catch (error) {
+    return res.send("Internal Server Error");
+ }
+  
 })
 
 router.get("/fetch" , fetchuser ,async (req,res)=>{
