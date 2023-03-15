284e0ea22510b07a81a12a523a87437e5abbb91b
BHuvisha Thakur <bhuvishathakur100@gmail.com>
2023-03-15 15:05:54 +0530

    I am checking the commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// console.log({data: process.env.USERID , VO: process.env.PASSWORD });
+console.log({data: process.env.USERID , VO: process.env.PASSWORD });
 
 // const mongoUrl = "mongodb://localhost:27017/pw"
 const mongoUrl = `mongodb+srv://${process.env.USERID}:${process.env.PASSWORD}@cluster0.evexpkx.mongodb.net/?retryWrites=true&w=majority`
