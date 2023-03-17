e5913825ffc5091f23f21a0bae67b9af45bf2841
aashutoshnishad147@gmail.com <ashutoshnishad2003@gmail.com>
2023-03-17 23:06:34 +0530

    adding log

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const mongoUrl = "mongodb://localhost:27017/pw"
-// const mongoUrl = `mongodb+srv://${process.env.USERID}:${process.env.PASSWORD}@cluster0.evexpkx.mongodb.net/?retryWrites=true&w=majority`
+// const mongoUrl = "mongodb://localhost:27017/pw"
+const mongoUrl = `mongodb+srv://${process.env.USERID}:${process.env.PASSWORD}@cluster0.evexpkx.mongodb.net/?retryWrites=true&w=majority`
 // console.log(mongoUrl);
 module.exports.connectDatabase = ()=>{
     mongoose
